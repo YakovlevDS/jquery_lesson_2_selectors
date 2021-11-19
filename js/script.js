ee2686608b1ce10275872f639ba3dda3f1cc21d9
Dmitriy Yakovlev <1d3ddd@gmail.com>
2021-11-19 18:10:18 +0200

    added  jquery selectors

---
+++ js/script.js
@@ -0,0 +1,57 @@
+jQuery(function($) {
+	
+	//$('input[name=name]').focus();
+	//$('input:focus').css({'border':'2px solid red'});;
+	//css({'border':'2px solid red'});;
+	
+	//$('div:first').css({'border':'2px solid red'});
+	//$('input:first').css({'border':'2px solid red'});
+	
+	//$('input:last').css({'border':'2px solid red'});
+	//$('div:last').css({'border':'2px solid red'});
+	
+	//$('input:eq(2)').css({'border':'2px solid red'});
+	
+	//$('input:not(input:eq(2))').css({'border':'2px solid red'});
+	
+	
+	//$('input:even').css({'border':'2px solid red'});//0 1 2 3 4 5
+	//$('li:odd').css({'border':'2px solid red'});//0 1 2 3 4 5
+	
+	
+	//$('input:gt(-3)').css({'border':'2px solid red'});//0 1 2 3 4 5
+	
+	//   -1
+	//$('.message:header').css({'border':'2px solid red'});//0 1 2 3 4 5
+	
+	//$('input:hidden').attr('type','text');//0 1 2 3 4 5
+	
+	//$('h2:lang(ru)').css({'border':'2px solid red'});;
+	
+	
+	//$('p:last-child').css({'border':'2px solid red'});
+	
+	//0 1 2 3 4 5 6
+	//$('p:nth-child(2n+2)').css({'border':'2px solid red'});
+	
+	//$('span:only-child').css({'border':'2px solid red'});
+	
+	//$('span:only-of-type').css({'border':'2px solid red'});
+	
+	//$('p:last-of-type').css({'border':'2px solid red'});
+	
+	//$('p:nth-of-type(2n+2)').css({'border':'2px solid red'});
+	
+	//$('p:contains("tristique")').css({'border':'2px solid red'});
+	//$('span:empty').css({'border':'2px solid red'});
+	
+	
+	$('h2:parent').css({'border':'2px solid red'});
+	
+	
+	
+	
+	
+	
+		
+});
